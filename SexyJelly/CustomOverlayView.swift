ee1c1f21f49ea0afe31a00967fd2b667055534b2
Wade <projectj@gmail.com>
2015-10-25 13:59:13 -0700

    Initial

---
+++ SexyJelly/CustomOverlayView.swift
@@ -0,0 +1,41 @@
+//
+//  CustomOverlayView.swift
+//  SexyJelly
+//
+//  Created by noun on 10.23.15.
+//  Copyright © 2015 noun. All rights reserved.
+//
+
+import UIKit
+import Koloda
+
+private let overlayRightImageName = "overlay_like"
+private let overlayLeftImageName = "overlay_skip"
+
+class CustomOverlayView: OverlayView {
+    
+    @IBOutlet lazy var overlayImageView: UIImageView! = {
+    
+        [unowned self] in
+        
+        var imageView = UIImageView(frame: self.bounds)
+        self.addSubview(imageView)
+        
+        return imageView
+        }()
+    
+    override var overlayState:OverlayMode  {
+        didSet {
+            switch overlayState {
+            case .Left :
+                overlayImageView.image = UIImage(named: overlayLeftImageName)
+            case .Right :
+                overlayImageView.image = UIImage(named: overlayRightImageName)
+            default:
+                overlayImageView.image = nil
+            }
+            
+        }
+    }
+    
+}
